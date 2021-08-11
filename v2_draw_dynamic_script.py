327cc0db2da6ca1f70dba451d5fcc12a30329b25
advx9600 <nwz88348307@163.com>
2021-08-11 09:40:53 +0800

    a

--- v2_draw_dynamic_script.py
+++ v2_draw_dynamic_script.py
@@ -8,13 +8,14 @@ def cmd(cmdstr):
     os.system(cmdstr)
     
 def main():    
+    '''
     if len(sys.argv) < 2:
         logging.error("please input msg log file")
         return
-        
+    '''        
     while True:
         try:
-            main_app.draw_dynamic(sys.argv[1])
+            main_app.draw_dynamic(r"D:\Programs\bin3(EC2)_R\log\010011112222\msg.log")
         except Exception as e:
             print("catch exception",e)
             logging.error(str(e))
---
+++ v2_run_script.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    os.chdir(r"D:\show_log")
+    os.system("dir")
+    os.system("py v2_draw_dynamic_script.py")
+    
+main()
